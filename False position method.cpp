570ed0ede3f76a31ad1a80b9ab85c5c357b88de8
Ariful Islam <156695945+Ariful-Islam010@users.noreply.github.com>
2025-07-04 23:33:12 +0600

    Update False position method.cpp

--- False position method.cpp
+++ False position method.cpp
@@ -1,71 +1,97 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Function definition: f(x) = 3x - cos(x) + 1
-double f(double x)
-{
+// Function definition: f(x) = 3x - cos(x) - 1
+double f(double x) {
     return 3 * x - cos(x) - 1;
 }
 
-// False Position Method with Absolute Error Calculation
-void falsePosition(double xl, double xu, int iterations)
-{
-    double xr = xl, xr_old, fxr, error = 0;
+// False Position Method
+void falsePosition(double xl, double xu, int maxIterations, double stopError, bool useError) {
+    double xr = xl, xr_old, fxr, error = 100;
 
     cout << fixed << setprecision(6);
-    cout << "Iter     xl            xu            xr            f(xr)         Error" << endl;
-    cout << "-------------------------------------------------------------------------" << endl;
+    cout << "Iter     xl            xu            xr            f(xr)         Error(%)" << endl;
+    cout << "--------------------------------------------------------------------------" << endl;
 
-    for (int i = 1; i <= iterations; i++)
-    {
+    for (int i = 1; i <= maxIterations; ++i) {
         xr_old = xr;
         xr = (xl * f(xu) - xu * f(xl)) / (f(xu) - f(xl));
         fxr = f(xr);
 
-        if (i > 1)
-        {
-            error = fabs(xr - xr_old);  // Absolute error
+        if (i > 1) {
+            error = fabs((xr - xr_old) / xr) * 100;
         }
 
         cout << left << setw(8) << i
              << setw(14) << xl
              << setw(14) << xu
              << setw(14) << xr
-             << setw(14) << fxr
-             << error << endl;
+             << setw(14) << fxr;
+
+        if (i == 1)
+            cout << "-";
+        else
+            cout << error;
+
+        cout << endl;
 
         if (fxr == 0.0)
-        {
             break;
-        }
         else if (f(xl) * fxr < 0)
-        {
             xu = xr;
-        }
         else
-        {
             xl = xr;
-        }
+
+        if (useError && i > 1 && error < stopError)
+            break;
     }
-    cout << "\nApproximate Root after " << iterations << " iterations: " << xr << endl;
+
+    cout << "\nApproximate Root: " << xr << endl;
 }
 
 // Main function
-int main()
-{
+int main() {
     double xl, xu;
-    int iterations;
+    int choice;
 
-    // Input interval and iterations
-    cout << "Enter lower bound (xl): ";
-    cin >> xl;
-    cout << "Enter upper bound (xu): ";
-    cin >> xu;
-    cout << "Enter number of iterations: ";
-    cin >> iterations;
+    // Ensure valid xl and xu
+    while (true) {
+        cout << "Enter lower bound (xl): ";
+        cin >> xl;
+        cout << "Enter upper bound (xu): ";
+        cin >> xu;
 
-    // Perform False Position Method
-    falsePosition(xl, xu, iterations);
+        if (f(xl) * f(xu) < 0)
+            break;
+        else
+            cout << "Invalid interval! f(xl) and f(xu) must have opposite signs.\n\n";
+    }
+
+    cout << "\nChoose an option:\n";
+    cout << "1. Fixed number of iterations\n";
+    cout << "2. Stop when Approximate Error is small enough\n";
+    cout << "Enter your choice (1 or 2): ";
+    cin >> choice;
+
+    switch (choice) {
+        case 1: {
+            int iterations;
+            cout << "Enter number of iterations: ";
+            cin >> iterations;
+            falsePosition(xl, xu, iterations, 0.0, false);
+            break;
+        }
+        case 2: {
+            double stopError;
+            cout << "Enter maximum approximate error (in %): ";
+            cin >> stopError;
+            falsePosition(xl, xu, 1000, stopError, true);  // max 1000 iterations
+            break;
+        }
+        default:
+            cout << "Invalid choice! Please enter 1 or 2.\n";
+    }
 
     return 0;
 }
